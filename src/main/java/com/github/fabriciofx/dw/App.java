95deb192032a5beae48daa17e1fe248e9cf80582
Fabricio Cabral <fabriciofx@gmail.com>
2016-10-25 18:27:45 -0300

    Read configs from MANIFEST.MF

--- src/main/java/com/github/fabriciofx/dw/App.java
+++ src/main/java/com/github/fabriciofx/dw/App.java
@@ -7,14 +7,26 @@ import java.net.URISyntaxException;
 import com.github.fabriciofx.dw.browser.Browser;
 import com.github.fabriciofx.dw.browser.Browsers;
 import com.github.fabriciofx.dw.web.WebServer;
+import com.jcabi.manifests.Manifests;
 
 public final class App {
 	public static void main(String[] args) throws InterruptedException {
 		try {
-			final Server server = new WebServer(8080);
+			final int port = Integer.parseInt(
+				Manifests.read("DesktopWeb-Port")
+			);
+			final Server server = new WebServer(port);
 			server.start();
 			final Browser browser = new Browsers(server).browser();
-			browser.open(new URI("http://localhost:8080"));
+			browser.open(
+				new URI(
+					String.format(
+						"http://%s:%d",
+						Manifests.read("DesktopWeb-Host"),
+						port
+					)
+				)
+			);
 		} catch (final IOException | URISyntaxException e) {
 			e.printStackTrace();
 		}
