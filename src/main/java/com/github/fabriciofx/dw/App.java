10d04658ec2fb8b5d8a8b3935cb00b0f6c97c92e
Fabricio Cabral <fabriciofx@gmail.com>
2017-01-26 22:13:38 -0300

    Changed to use config from rocket

--- src/main/java/com/github/fabriciofx/dw/App.java
+++ src/main/java/com/github/fabriciofx/dw/App.java
@@ -7,10 +7,10 @@ import java.util.concurrent.CountDownLatch;
 
 import com.github.fabriciofx.dw.browser.Browser;
 import com.github.fabriciofx.dw.browser.Browsers;
-import com.github.fabriciofx.dw.util.Config;
-import com.github.fabriciofx.dw.util.ConfigFile;
 import com.github.fabriciofx.dw.web.WebServer;
 import com.github.fabriciofx.dw.web.WebServerProcess;
+import com.github.fabriciofx.rocket.config.Config;
+import com.github.fabriciofx.rocket.config.ConfigFile;
 import com.jcabi.log.Logger;
 
 
@@ -18,8 +18,8 @@ public final class App {
 	public static void main(String[] args) {
 		try {
 			final Config config = new ConfigFile("desktop-web.properties");
-			final String host = config.read("desktop-web.host");
-			final int port = Integer.parseInt(config.read("desktop-web.port"));
+			final String host = config.value(String.class, "desktop-web.host");
+			final int port = config.value(Integer.class, "desktop-web.port");
 			// TODO: remove temporal coupling between server and browser
 			final CountDownLatch cdl = new CountDownLatch(1);
 			final Server server = new WebServer(cdl,
